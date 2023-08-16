f5dbff33d9cd8cda3cdcf2d09e0ddb1afff7248b
Kawther Ahmed <kawtherahmed556@gmail.com>
2023-08-16 15:03:21 +0300

    edited the abbout

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-const about = "I am a computer science graduate who is passionate about Data. Currently, I'm a BI trainee at ITI. I'm also a graduate of Data Analysis Professional & Advanced Nanodegrees from Udacity. I have good experience in Python, advanced SQL, and SQL BI with more than 10 projects. I'm well-skilled in data analysis and reporting using Power BI and Tableau.";
+const about = "I am a computer science graduate who is passionate about Data. Recently, I graduated from the ITI (BI track). I'm also a graduate of Data Analysis Professional & Advanced Nanodegrees from Udacity through the FWD initiative. I have good experience in Python, advanced SQL, and SQL BI with more than 15 projects. I'm well-skilled in data analysis and reporting using Power BI and Tableau.";
 
 const imgs_root = "images/";
 const profile_img = imgs_root + "profile_img.jpeg"
